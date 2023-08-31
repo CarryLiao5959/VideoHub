0b415f18dd3169c3fa1d00fb1dde6e07ced0636a
Kun Lei <stevenlei617@gmail.com>
2023-08-31 10:49:13 +0000

    add upload button

--- backend/RESTful/videolist.py
+++ backend/RESTful/videolist.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, abort,send_from_directory,render_template
+from flask import Flask, jsonify, abort,render_template
 from flask_cors import CORS,cross_origin
 from flask import request
 import json
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder='/home/engage/github_projects/socket/backend/static')
 CORS(app)
+
+UPLOAD_FOLDER = '/home/engage/github_projects/socket/backend/static' 
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'flv'} 
 
 @app.route('/videos', methods=['GET'])
 @cross_origin()
@@ -45,6 +50,29 @@ def video_page():
 @cross_origin()
 def video_list():
     return render_template('videolist.html')
+
+# 检查文件是否允许上传
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+@cross_origin()
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return jsonify({"message": "File uploaded successfully", "filepath": filepath}), 200
+    else:
+        return jsonify({"error": "File type not allowed"}), 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
