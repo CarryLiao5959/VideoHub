82ad6bf90fb4ecc8e9f5fcaec5e77a96a9ee5aad
Kun Lei <stevenlei617@gmail.com>
2023-08-31 09:42:35 +0000

    add barrage frame

--- backend/RESTful/videolist/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__, static_folder='../../static')
-CORS(app)
-
-# 确保路由在app创建后被加载
-from videolist import routes
--- backend/RESTful/videolist/main.py
+++
@@ -1,4 +0,0 @@
-from videolist import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
